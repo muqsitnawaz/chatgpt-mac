2fc031dd345cf9fe595e3db3db326bf91eaf3013
Muqsit Nawaz <muqsitnawaz@gmail.com>
2023-12-18 10:22:41 -0800

    feat: set default model to the gpt-4

--- forge.config.js
+++ forge.config.js
@@ -4,23 +4,23 @@ module.exports = {
     name: "ChatGPT",
     executableName: "ChatGPT",
     icon: "images/icon",
-    appBundleId: "com.vincelwt.chatgptmac",
-    extendInfo: {
-      LSUIElement: "true",
-    },
-    osxSign: {
-      hardenedRuntime: false,
-      gatekeeperAssess: false,
-      identity: "Developer ID Application: Lyser.io Ltd (R4PF6TTR6Z)",
-    },
-    osxNotarize: {
-      appBundleId: "com.vincelwt.chatgptmac",
+    // appBundleId: "com.vincelwt.chatgptmac",
+    // extendInfo: {
+    //   LSUIElement: "true",
+    // },
+    // osxSign: {
+    //   hardenedRuntime: false,
+    //   gatekeeperAssess: false,
+    //   identity: "Developer ID Application: Lyser.io Ltd (R4PF6TTR6Z)",
+    // },
+    // osxNotarize: {
+    //   appBundleId: "com.vincelwt.chatgptmac",
 
-      tool: "notarytool",
-      appleId: parsed.APPLE_ID,
-      appleIdPassword: parsed.APPLE_PASSWORD,
-      teamId: parsed.APPLE_TEAM_ID,
-    },
+    //   tool: "notarytool",
+    //   appleId: parsed.APPLE_ID,
+    //   appleIdPassword: parsed.APPLE_PASSWORD,
+    //   teamId: parsed.APPLE_TEAM_ID,
+    // },
   },
   publishers: [
     {
